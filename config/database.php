be0327f2fd934d5665f572720886e8eaa333706e
SameerLeopard <sameer.qureshi@leopardscourier.com>
2026-08-30 14:07:10 +0500

    Add automatic SSL certificate detection for TiDB Cloud and cloud MySQL

--- config/database.php
+++ config/database.php
@@ -60,7 +60,11 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                Mysql::ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA') ?: (
+                    file_exists('/etc/pki/tls/certs/ca-bundle.crt')
+                        ? '/etc/pki/tls/certs/ca-bundle.crt'
+                        : (file_exists('/etc/ssl/certs/ca-certificates.crt') ? '/etc/ssl/certs/ca-certificates.crt' : null)
+                ),
             ]) : [],
         ],
 
