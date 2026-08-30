3bb3dc72c9e4d256061ada6eac3770482e59a8b6
sameerqureshi03311 <sameer.qureshi0331@gmail.com>
2026-08-30 14:41:16 +0500

    Enable APP_DEBUG=true and verbose runtime error reporting for Vercel

--- api/index.php
+++ api/index.php
@@ -1,5 +1,10 @@
 <?php
 
+// Enable explicit runtime error reporting for debugging on serverless
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
 $storagePath = '/tmp/storage';
 $bootstrapCachePath = '/tmp/bootstrap/cache';
 
